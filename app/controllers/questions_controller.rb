0301e620e56e12d9ed7df7ed3d31e0d61a84dcdf
Nicoleta Grubi <msnicoleta@outlook.com>
2023-08-14 13:19:26 +0100

    adding the logic to che

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,20 @@
+class QuestionsController < ApplicationController
+  def aks
+  end
+
+
+  def answer
+    if request.post? # Handle POST request
+      user_message = params[:question]
+      if user_message.blank?
+        @coach_answer = "Please ask a question."
+      elsif user_message == "I am going to work"
+        @coach_answer = "Great!"
+      elsif user_message.end_with?("?")
+        @coach_answer = "Silly question, get dressed and go to work!"
+      else
+        @coach_answer = "I don't care, get dressed and go to work!"
+      end
+    end
+  end
+end
